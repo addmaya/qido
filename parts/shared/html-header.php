db8d744584531369554e98a17350f9765d65d312
Addmaya <admin@addmaya.com>
2017-06-22 16:28:11 +0300

    SASS/CSS Development

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -12,7 +12,7 @@
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico"/>
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Six+Caps" rel="stylesheet">
-		<link href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" rel="stylesheet">
+		<link href="<?php echo get_stylesheet_directory_uri(); ?>/style.css?v=0.5" rel="stylesheet">
 		<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/build/vendors.js"></script>
 		<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/build/app.js"></script>
 		<?php wp_head(); ?>
